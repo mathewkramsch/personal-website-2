9eab1724ebbe530a230410942b9589bbb55bddc8
Mathew Kramsch <mathewkramsch@ucsb.edu>
2021-08-22 11:39:59 -0700

    added fontawesome kit script

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,7 @@ export default function App({ Component, pageProps }) {
 				<link rel="preconnect" href="https://fonts.googleapis.com"/>
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
 				<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,500;1,600&display=swap" rel="stylesheet"/>
+				<script src="https://kit.fontawesome.com/e82fae02e2.js" crossorigin="anonymous"></script>
 			</Head>
 			<Component {...pageProps} />
 		</>
