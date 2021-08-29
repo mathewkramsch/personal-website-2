761f0dacfd979ce14a5983df4c5eeb0dc92a0835
Mathew Kramsch <mathewkramsch@ucsb.edu>
2021-08-29 13:56:07 -0700

    connected contact form to email

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fcafdcde85eda7d83374"; }
+/******/ 		__webpack_require__.h = function() { return "6538b504c3c8d0e7a149"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/contact/comps/emailInput.jsx
+++ components/contact/comps/emailInput.jsx
@@ -4,47 +4,53 @@ import React, { useState } from 'react'
 import s from '../../../styles/layout/contact.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import emailjs from 'emailjs-com'
 
-const sendEmailButton = (status, toggleStatus)=>{
-    let submitButtonChild = (
-        <>
-            <h4 className='italic footerText'>send email</h4>
-            <FontAwesomeIcon icon={faPaperPlane} className='icon' id='plane'/>
-        </>
-    );
-    if (status==='sent') submitButtonChild = (
-        <>
-            <h4 className='italic footerText emailSent'>jk this doesn't work yet</h4>
-        </>
-    );
-
+const sendEmailButton = (status)=>{
+    let submitValue = 'send email'
+    if (status==='sent') submitValue = 'message sent'
     return (
-        <div className='submitButton' onClick={toggleStatus}>
-            { submitButtonChild }
+        <div className='submitButton'>
+            <input type='submit' value={submitValue} className='italic footerText'/>
+            <FontAwesomeIcon icon={faPaperPlane} className='icon' id='plane'/>
         </div>
     );
 }
 
 export default function EmailInput() {
     const [status, setStatus] = useState('notSent');
-    const toggleStatus = ()=>{
-        if (status==='notSent') setStatus('sent');
-        else setStatus('notSent');
+    const sendEmail = (e)=>{
+        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+
+        console.log(e.target.from_name.value);
+        if (!e.target.from_name.value) { alert('need a name'); }
+        if (!e.target.from_email.value) { alert('need an email address'); }
+        if (!e.target.message.value) { alert('need a message'); }
+        if (e.target.from_name.value && e.target.from_email.value && e.target.message.value) {
+            emailjs.sendForm('service_ooit997','template_69kd92e',e.target,'user_FCqOc9EcSn7381ssMYLsf')
+            .then(
+                (result) => {
+                    setStatus('sent');
+                    setTimeout(()=>{ window.location.reload(); }, 1000);
+                },
+                (error) => { console.log(error.text); }
+            );
+        }
     }
 
     return (
-        <form action='#' className={s.inputs}>
+        <form action='#' className={s.inputs} onSubmit={sendEmail}>
             <div className={s.textInputs}>
-                <input type="text" id="name" name="fname" className='inputText'
+                <input type="text" id="name" name="from_name" className='inputText'
                     placeholder='Name'/>
-                <input type="text" id="email" name="fname" className='inputText'
+                <input type="text" id="email" name="from_email" className='inputText'
                     placeholder='Email Address'/>
                 <textarea name="message" rows="12" cols="30" className='inputText'
                     placeholder='Your message to mathewkramsch@ucsb.edu'>
                 </textarea>
             </div>
-            <div className='submitButton' onClick={toggleStatus}>
-                { sendEmailButton(status,toggleStatus) }
+            <div>
+                { sendEmailButton(status) }
             </div>
         </form>
     );
