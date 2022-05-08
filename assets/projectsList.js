c312759066615454ba999f38bec26149ee046441
Mathew Kramsch <mathewkramsch@ucsb.edu>
2022-05-07 22:30:57 -0700

    added vim todo notes, small stylistic changes

--- assets/projectsList.js
+++ assets/projectsList.js
@@ -3,11 +3,11 @@
 export const projectsList = [  // array of project objects
     {
         title: 'TuffHouse Store/Blog Website',
-        description: 'Worked as sole front-end developer for local start-up to design and build a responsive and scalable online store/blog from scratch using React and SASS. ',
+        description: 'Worked as sole front-end developer to design and build an online store/blog for TuffHouse using React, Next.js, and SASS. ',
         imgSrc: '/projectImgs/tuffhouse.png',
 		link: 'https://tuffhouse.us/',
         srcCode: 'false',
-        srcCodeLink: 'https://github.com/mathewkramsch/odin-etch-a-sketch'
+        srcCodeLink: ''
     },
     {
         title: 'IV Housing Web Application',
@@ -16,5 +16,13 @@ export const projectsList = [  // array of project objects
         link: 'https://cs48-s20-s0-t3-prod.herokuapp.com/',
         srcCode: 'true',
         srcCodeLink: 'https://github.com/ucsb-cs48-s20/project-s0-t3-iv-housing'
+    },
+    {
+        title: 'Vim To Do Notetaking',
+        description: 'Simple vim-script for taking organization/school notes in vim',
+        imgSrc: '/projectImgs/todo.png',
+        link: 'https://github.com/mathewkramsch/vim_todo',
+        srcCode: 'false',
+        srcCodeLink: ''
     }
 ];
